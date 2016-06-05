3b191aa4292652124f792b39dcf881bbf02f665e
Bryan T. Meyers <btmeme@rit.edu>
2016-06-05 15:55:29 -0400

    ADDED: testing for edges

--- core.go
+++ core.go
@@ -2,11 +2,15 @@ package undot
 
 type Undot struct {
 	Clusters map[string]*Cluster
-	Edges    map[string]*Edge
+	Edges    map[string][]*Edge
+}
+
+func NewUndot() *Undot{
+	return &Undot{make(map[string]*Cluster), make(map[string][]*Edge)}
 }
 
 func Parse(dot string) (*Undot, error) {
-	u := &Undot{make(map[string]*Cluster), make(map[string]*Edge)}
+	u := NewUndot()
 	dot = ParseEdges(dot, u)
 	dot, err := ParseClusters(dot, u)
 	return u, err
---
+++ edge_test.go
@@ -0,0 +1,121 @@
+package undot
+
+import "testing"
+
+func TestParseEdgesEmpty(t *testing.T) {
+	u := NewUndot()
+	edgestring := ""
+	dot := ParseEdges(edgestring, u)
+	if len(u.Edges) != 0 {
+		t.Error("Should be no edges")
+	}
+	if len(dot) != 0 {
+		t.Error("dot string should be empty")
+	}
+}
+
+func TestParseEdgesSingle(t *testing.T) {
+	u := NewUndot()
+	edgestring := "A -> B;"
+	dot := ParseEdges(edgestring, u)
+	if len(u.Edges) != 1 {
+		t.Error("Should be one edge")
+	}
+	if len(u.Edges["A"]) != 1 {
+		t.Error("Should only be one Edge for 'A'")
+	}
+	if u.Edges["A"][0].NextNode != "B" {
+		t.Error("Edge did not go from A to B")
+	}
+	if len(dot) != 0 {
+		t.Error("dot string should be empty")
+	}
+}
+
+func TestParseEdgesSingleAttributes(t *testing.T) {
+	u := NewUndot()
+	edgestring := "A -> B [label=\"1234 | bob\"];"
+	dot := ParseEdges(edgestring, u)
+	if len(u.Edges) != 1 {
+		t.Error("Should be one edge")
+	}
+	if len(u.Edges["A"]) != 1 {
+		t.Error("Should only be one Edge for A")
+	}
+	if u.Edges["A"][0].NextNode != "B" {
+		t.Error("Edge did not go from A to B")
+	}
+	if len(u.Edges["A"][0].Attributes) != 1 {
+		t.Error("Missing attributes for edge 'A -> B'")
+	}
+	if u.Edges["A"][0].Attributes["label"] != "1234 | bob" {
+		t.Errorf("Found wrong attributes for 'A', '%s'", u.Edges["A"][0].Attributes["label"])
+	}
+	if len(dot) != 0 {
+		t.Error("dot string should be empty")
+	}
+}
+
+func TestParseEdgesMultiple(t *testing.T) {
+	u := NewUndot()
+	edgestring := "A -> B;\n B <- C;\n C -> D;"
+	dot := ParseEdges(edgestring, u)
+	if len(u.Edges) != 2 {
+		t.Error("Should be two sets of edges")
+	}
+	if len(u.Edges["A"]) != 1 {
+		t.Error("Should be a single EDGE for 'A'")
+	}
+	if u.Edges["A"][0].NextNode != "B" {
+		t.Error("Edge did not go from A to B")
+	}
+	if len(u.Edges["B"]) != 0 {
+		t.Error("Should not have found edges for 'B'")
+	}
+	if len(u.Edges["C"]) != 2 {
+		t.Error("Should have found two edges for 'C'")
+	}
+	if u.Edges["C"][0].NextNode != "B" {
+		t.Error("Edge did not go from C to B")
+	}
+	if u.Edges["C"][1].NextNode != "D" {
+		t.Error("Edge did not go from C to D")
+	}
+	if len(dot) != 0 {
+		t.Error("dot string should be empty")
+	}
+}
+
+func TestParseEdgesMultipleAttributes(t *testing.T) {
+	u := NewUndot()
+	edgestring := "A -> B;\n B <- C [label=1234];\n C -> D;"
+	dot := ParseEdges(edgestring, u)
+	if len(u.Edges) != 2 {
+		t.Error("Should be two sets of edges")
+	}
+	if len(u.Edges["A"]) != 1 {
+		t.Error("Should be a single EDGE for 'A'")
+	}
+	if u.Edges["A"][0].NextNode != "B" {
+		t.Error("Edge did not go from A to B")
+	}
+	if len(u.Edges["B"]) != 0 {
+		t.Error("Should not have found edges for 'B'")
+	}
+	if len(u.Edges["C"]) != 2 {
+		t.Error("Should have found two edges for 'C'")
+	}
+	if u.Edges["C"][0].NextNode != "B" {
+		t.Error("Edge did not go from C to B")
+	}
+	if u.Edges["C"][0].Attributes["label"] != "1234" {
+		t.Error("B's attributes did not match")
+	}
+	if u.Edges["C"][1].NextNode != "D" {
+		t.Error("Edge did not go from C to D")
+	}
+	if len(dot) != 0 {
+		t.Error("dot string should be empty")
+	}
+}
+
