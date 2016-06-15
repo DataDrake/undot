d93f940f9aa79191856091de2fcddee072d5c07e
btmeme <btmeme@rit.edu>
2016-06-15 10:13:04 -0400

    ADDED: now able to get any node by name

--- undot.go
+++ undot.go
@@ -9,6 +9,16 @@ func NewUndot() *Undot {
 	return &Undot{make(map[string]*Cluster), make(map[string][]*Edge)}
 }
 
+func (u *Undot)GetNodeByName(name string ) *Node {
+	for _,c := range u.Clusters {
+		n := c.Nodes["name"]
+		if n != nil {
+			return n
+		}
+	}
+	return nil
+}
+
 func Parse(dot string) (*Undot, error) {
 	u := NewUndot()
 	dot = ParseEdges(dot, u)
